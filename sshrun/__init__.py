eb2e339c6c6b4d2ec434543f7db8cabf8c7f80de
Dickon Reed <Dickon.Reed@citrix.com>
2012-11-02 17:21:08 +0000

    add missing __init__.py add retry_until_true

---
+++ sshrun/__init__.py
@@ -0,0 +1,7 @@
+from run import run, isfile, statcheck, isdir, readfile, writefile, islink
+from run import specify, maketempfile, maketempdirectory, SubprocessError
+from run import TimeoutError
+from retry import retry
+from time_limit import time_limit
+from timed import Timed
+
--- sshrun/retry.py
+++ sshrun/retry.py
@@ -1,6 +1,11 @@
 from time import time, sleep
+from sshrun.time_limit import TimeoutError
 
-def retry(fn, description, pace=1.0, timeout=60.0, catch=[Exception], propagate=[]):
+class TimeoutStillFalseError(TimeoutError):
+    """A function is still returning false after too long"""
+
+def retry(fn, description, pace=1.0, timeout=60.0, catch=[Exception], 
+          propagate=[]):
     """Run fn, retrying in the event of a exceptions on the catch
     list for up to timeout seconds, waiting pace seconds between attempts"""
     start_time = time()
@@ -26,3 +31,44 @@ def retry(fn, description, pace=1.0, timeout=60.0, catch=[Exception], propagate=
         print 'RETRY: sleeping', pace, 'seconds after iteration', count, \
             'of', description, 'failed'
         sleep(pace)
+
+def retry_until_true(fn, description='seeking truth', pace=1.0, timeout=60.0, 
+                     false_callback=lambda: pass, verbose=False):
+    """Keep running function until it returns a truish value.
+
+    Arguments:
+
+      fn: the function we wish to return true
+      description: text describing what we are doing
+      pace: number of seconds to pace out attempts to call fn
+      timeout: give up aftr this number of seconds 
+      false_callback: function to call if fn is false
+
+    Returns:
+      
+      the result of fn, which will be a true value
+       
+    Raises:
+
+      TimeoutStillFalseError
+    """
+
+    start_time= time()
+    count = 0
+    while 1:
+        count += 1
+            
+        print 'RETRY:', description, 'iteration', count, 'timeout', timeout
+        
+        result = fn()
+        delta_t = time() - start_time
+        print 'RETRY:', description, 'truish' if result else 'falsish', 'on iteration', count,
+        if result:
+            print
+            return result
+        else:
+            print 'will', 'RETRY' if delta_t < timeout else 'STOP'
+        if delta_t >= timeout:
+            raise TimeoutStillFalseError(
+        sleep(pace)
+        
