9abd42c896eb546983eff7e0606747d2c1333431
Dickon Reed <Dickon.Reed@citrix.com>
2012-11-03 08:36:07 +0000

    reduce verbosity

--- sshrun/retry.py
+++ sshrun/retry.py
@@ -1,11 +1,12 @@
+"""Retry functions"""
 from time import time, sleep
 from sshrun.time_limit import TimeoutError
 
 class TimeoutStillFalseError(TimeoutError):
     """A function is still returning false after too long"""
 
-def retry(fn, description, pace=1.0, timeout=60.0, catch=[Exception], 
-          propagate=[]):
+def retry(fn, description, pace=1.0, timeout=60.0, 
+          catch=[Exception], propagate=[], verbose=False):
     """Run fn, retrying in the event of a exceptions on the catch
     list for up to timeout seconds, waiting pace seconds between attempts"""
     start_time = time()
@@ -13,27 +14,31 @@ def retry(fn, description, pace=1.0, timeout=60.0, catch=[Exception],
     while 1:
         count += 1
         delta_t = time() - start_time
-        print 'RETRY:', description, 'iteration', count, 'used', delta_t,
-        print 'timeout', timeout
+        if verbose:
+            print 'RETRY:', description, 'iteration', count, 'used', delta_t,
+            print 'timeout', timeout
         try:
             result = fn()
-            print 'RETRY:', description, 'iteration', count, 'succeeded'
+            if verbose:
+                print 'RETRY:', description, 'iteration', count, 'succeeded'
             return result
         except Exception, exc:
             matches = [x for x in catch if isinstance(exc, x)]
             propagates = [x for x in propagate if isinstance(exc, x)]
             delta_t = time() - start_time
             if delta_t < timeout and matches and not propagates:
-                print 'RETRY:', description, 'iteration', count, \
-                    'failed with', repr(exc )
+                if verbose:
+                    print 'RETRY:', description, 'iteration', count,
+                    print 'failed with', repr(exc )
             else:
                 raise
-        print 'RETRY: sleeping', pace, 'seconds after iteration', count, \
-            'of', description, 'failed'
+        if verbose:
+            print 'RETRY: sleeping', pace, 'seconds after iteration', count,
+            print 'of', description, 'failed'
         sleep(pace)
 
 def retry_until_true(fn, description='seeking truth', pace=1.0, timeout=60.0, 
-                     false_callback=lambda: pass, verbose=False):
+                     false_callback=lambda: None, verbose=False):
     """Keep running function until it returns a truish value.
 
     Arguments:
@@ -58,17 +63,19 @@ def retry_until_true(fn, description='seeking truth', pace=1.0, timeout=60.0,
     while 1:
         count += 1
             
-        print 'RETRY:', description, 'iteration', count, 'timeout', timeout
-        
-        result = fn()
+        if verbose:
+            print 'RETRY:', description, 'iteration', count, 
+            print 'timeout', timeout
+
+        with time_limit(timeout-delta_t, 'run '+description):
+            result = fn()
         delta_t = time() - start_time
-        print 'RETRY:', description, 'truish' if result else 'falsish', 'on iteration', count,
+        if verbose:
+            print 'RETRY:', description, 'truish' if result else 'falsish', 
+            print 'on iteration', count
         if result:
             print
             return result
-        else:
-            print 'will', 'RETRY' if delta_t < timeout else 'STOP'
-        if delta_t >= timeout:
-            raise TimeoutStillFalseError(
+
         sleep(pace)
         
